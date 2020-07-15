a9207643d699f55e4b34edbbe753e5fd055736b4
Jean Lima de Souza Junior <jean.jr.souza@hotmail.com>
2020-07-15 15:43:56 -0300

    Utilizando recurso de lambda para percorrer uma lista

--- src/Lambda/CalculoLambda2.java
+++ src/Lambda/CalculoLambda2.java
@@ -6,7 +6,7 @@ public class CalculoLambda2 {
 
     public static void main(String[] args) {
         /**
-         * Binaryoperator = represeta uma operação sobre dois operadores do mesmo tipo.
+         * Binaryoperator = é uma interface do java que represeta uma operação sobre dois operadores do mesmo tipo.
          * com isso não é necessario criar a interface "Calculo"
          */
         BinaryOperator<Double> calc = (x, y) -> x + y;
