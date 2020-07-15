e4cda7e0531e3678b109308bbe50d2b4c335629c
Jean Lima de Souza Junior <jean.jr.souza@hotmail.com>
2020-07-15 15:00:48 -0300

    Primeiro teste com Lambda

---
+++ src/Lambda/CalculoLambda.java
@@ -0,0 +1,13 @@
+package Lambda;
+
+public class CalculoLambda {
+
+    public static void main(String[] args) {
+        // referencia ao metedo de calculo que recebe 2 parametros
+        Calculo calc = (x, y) -> {
+            return x + y;
+        };
+
+        System.out.println(calc.executa(5, 4));
+    }
+}
