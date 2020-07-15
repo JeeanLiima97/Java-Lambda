d252d33c5b6d86277244485d1d08f6c3eeebc30a
Jean Lima de Souza Junior <jean.jr.souza@hotmail.com>
2020-07-15 14:55:14 -0300

    Criação do projeto

---
+++ src/Lambda/Calculo.java
@@ -0,0 +1,6 @@
+package Lambda;
+
+public interface Calculo {
+
+    double executa(double a, double b);
+}
---
+++ src/Lambda/Multiplicar.java
@@ -0,0 +1,8 @@
+package Lambda;
+
+public class Multiplicar implements Calculo {
+    @Override
+    public double executa(double a, double b) {
+        return a * b;
+    }
+}
